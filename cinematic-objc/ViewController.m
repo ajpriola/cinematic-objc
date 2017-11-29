1834e07598997a6e71b280b5f6c63b56f2db5ce5
AJ Priola <ajpriola@gmail.com>
2017-11-29 10:56:47 -0500

    Initial Commit

---
+++ cinematic-objc/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  cinematic-objc
+//
+//  Created by AJ Priola on 11/29/17.
+//  Copyright © 2017 AJ Priola. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
